6632a6bc264f7872d950608ba2a2ca9a0f9b0082
«Kamila» <milo4-ka01@yandex.ru>
2022-03-08 20:36:34 +0300

    Задание 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿void Square( int c, int d)
+{
+    if ( c*c == d)
+    {
+        Console.WriteLine($"{d} является квадратом {c}");
+    }
+    else if ( d*d == c )
+    {
+        Console.WriteLine($"{c} является квадратом {d}");
+    }
+    else Console.WriteLine($"Не являются");
+}
+
+Console.WriteLine("Введите два числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Square( a, b);
