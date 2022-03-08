2c82ae787b526f5a5c9b24036943f8d5bd1addba
«Kamila» <milo4-ka01@yandex.ru>
2022-03-08 12:55:10 +0300

    1-е, 2-е задание

---
+++ Task_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintNumb( int b )
+{
+    int i = -b;
+    while ( i < b + 1 )
+    {
+        Console.Write( i + " " );
+        i++;
+    }
+}
+void PrintChet( int b )
+{
+    int i = 0;
+    while ( i < b + 1)
+    {
+        if ( i % 2 == 0 )
+        {
+            Console.Write( i + " " );
+        }
+        i++;
+    }
+}
+
+Console.WriteLine("Введите число от 0 до 10");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumb(n);
+Console.WriteLine();
+PrintChet(n);
