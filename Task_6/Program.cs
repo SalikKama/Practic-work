022a85e431688adca89e0c03267fac3595c0dc57
SalikKama <100031271+SalikKama@users.noreply.github.com>
2022-03-08 21:26:55 +0300

    Update Program.cs

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,4 +1,5 @@
-﻿double Distance( int a1, int b1, int c1, int a2, int b2, int c2)
+//Найти расстояние между точками в пространстве 2D. 3D﻿
+double Distance( int a1, int b1, int c1, int a2, int b2, int c2)
 {
     double S = Math.Sqrt(Math.Pow((a2 - a1),2) + Math.Pow((b2 - b1),2) + Math.Pow((c2 - c1),2));
     return S;
