65755fd2199516c046f2f271f7ae84186f26c09c
«Kamila» <milo4-ka01@yandex.ru>
2022-03-08 20:40:35 +0300

    Задание 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,16 @@
+﻿int Chetvert(int X, int Y)
+{
+    int num = 0;
+    if (X > 0 && Y > 0) num = 1;
+    else if (X < 0 && Y > 0) num = 2;
+    else if (X < 0 && Y < 0) num = 3;
+    else num = 4;
+    return num;
+}
+
+Console.WriteLine("Введите числа x, y не равные нулю:");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+int number = Chetvert(x, y);
+Console.WriteLine($"Четверть номер: {number}");
