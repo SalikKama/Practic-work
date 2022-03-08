34dbf21d62719f9a7e04533afdd62c342545a78e
«Kamila» <milo4-ka01@yandex.ru>
2022-03-08 20:18:51 +0300

    Задание 2

---
+++ Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Weekend( string[] collect, int a)
+{
+    if ( collect[a-1] == "Suturday" )
+    {
+        Console.WriteLine("Выходной");
+    }
+    else if ( collect[a-1] == "Sunday" )
+    {
+        Console.WriteLine("Выходной");
+    }
+}
+Console.WriteLine("Введите число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Suturday", "Sunday"};
+
+Weekend(array, n);
